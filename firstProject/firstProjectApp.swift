87e3813c787ff1d86c9409acc999c10815a00137
Big Guy <milleraustin139@gmail.com>
2025-11-07 19:45:08 -0600

    Initial Commit

---
+++ firstProject/firstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstProjectApp.swift
+//  firstProject
+//
+//  Created by Big Guy on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct firstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
