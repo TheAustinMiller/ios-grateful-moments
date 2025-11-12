42a939f0530fca3d3d6d2b4c082daa5a1f7e443f
Big Guy <milleraustin139@gmail.com>
2025-11-11 20:46:37 -0600

    First try

---
+++ GratefulMoments/GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,22 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Big Guy on 11/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GratefulMomentsApp: App {
+    let dataContainer = DataContainer()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(dataContainer)
+        }
+        .modelContainer(dataContainer.modelContainer)
+    }
+}
